fe8e2281231e7946009a4c99428ed46c4e9c9bb8
Macko <lyson.maciej@gmail.com>
2021-09-02 02:26:41 +0200

    added stm32 project

---
+++ PlantNode/include/main.h
@@ -0,0 +1,17 @@
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "stm32f1xx_hal.h"
+
+  void Error_Handler(void);
+  void SystemClock_Config(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
