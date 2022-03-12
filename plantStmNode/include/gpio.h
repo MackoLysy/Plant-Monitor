b185578e68e119c7ef1054906750c30f69ce74c6
Macko <lyson.maciej@gmail.com>
2022-03-12 02:10:11 +0100

    removed stm from repo

--- plantStmNode/include/gpio.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "config.h"
-
-#define LED_PIN GPIO_PIN_13
-#define LED_GPIO_PORT GPIOB
-
-void gpio_init();
--- plantStmNode/include/uart.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "stm32f1xx_hal.h"
-
-void uart_init(UART_HandleTypeDef *huart);
